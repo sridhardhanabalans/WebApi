677fb26b19a4f17c1174074b493c9e4ec04c0e58
sridhardhanabalans <40361636+sridhardhanabalans@users.noreply.github.com>
2019-02-13 14:01:20 +0530

    webAPI Files added
    webAPI Files added

---
+++ Assignment1_Test/Models/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment1_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_Api.Models.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        DALLayer dalLayer = new DALLayer();
+
+        [TestMethod()]
+        public void GetSuppliersTest()
+        {
+            var result=dalLayer.GetSuppliers().Count;
+
+            Assert.IsTrue(result>0);
+        }
+
+        [TestMethod()]
+        public void GetItemsTest()
+        {
+            var result = dalLayer.GetItems().Count;
+
+            Assert.IsTrue(result > 0);
+        }
+
+        [TestMethod()]
+        public void GetPOMasterTest()
+        {
+            var result = dalLayer.GetPOMaster().Count;
+
+            Assert.IsTrue(result > 0);
+        }
+
+        [TestMethod()]
+        public void GetPODetailsTest()
+        {
+            var result = dalLayer.GetPODetails().Count;
+
+            Assert.IsTrue(result > 0);
+        }
+
+        [TestMethod()]
+        public void AddPOMasterTest()
+        {
+            POMaster value = new POMaster();
+            value.PONO = "6";
+            value.PODATE = DateTime.Now.AddDays(-6);
+            value.SUPLNO = "4";
+            var result=dalLayer.AddPOMaster(value);
+            Assert.IsTrue(result == 1);
+        }
+
+        [TestMethod()]
+        public void DeletePOMasterTest()
+        {
+            var result = dalLayer.DeletePOMaster("5");
+            Assert.IsTrue(result == 2);
+        }
+    }
+}
---
+++ Assignment1_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Assignment1_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assignment1_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ef9625e-57e0-4546-8933-4302e1e9570e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
